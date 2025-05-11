60c8fbb33e57d8eace0b687779baf74f80b68fc5
miriyan19425 <miriyan19425@gmail.com>
2025-05-11 22:38:00 +0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ function NavBar() {
     <nav>
       <ul className='navigation'>
         <li><NavLink to='/' className='navig'>Home</NavLink></li>
-		    <li><NavLink to='/physics' className='navig'>Physics</NavLink></li>
-		    <li><NavLink to='/chemistry' className='navig'>Chemistry</NavLink></li>
+	<li><NavLink to='/physics' className='navig'>Physics</NavLink></li>
+	<li><NavLink to='/chemistry' className='navig'>Chemistry</NavLink></li>
         <li><NavLink to='/medicine' className='navig'>Medicine</NavLink></li>
         <li><NavLink to='/literature' className='navig'>Literature</NavLink></li>
         <li><NavLink to='/peace' className='navig'>Peace</NavLink></li>
