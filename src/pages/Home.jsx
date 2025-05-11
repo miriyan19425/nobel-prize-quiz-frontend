7ed883309bf264fd80531c9953d53d90788cc39b
miriyan19425 <miriyan19425@gmail.com>
2025-05-11 22:36:23 +0300

    Update Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar';
+
+function Home() {
+	return ( 
+    <header>
+			<NavBar />
+			<h1>Nobel Prize Quizes</h1>	
+		</header>
+	);	
+}
+
+export default Home;
