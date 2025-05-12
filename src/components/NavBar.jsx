9ac37b065051073138169f7f50018aef6581c5e7
miriyan19425 <miriyan19425@gmail.com>
2025-05-12 22:58:06 +0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  function toggleDropdown(){
+    setDropdownOpen(prev => !prev);
+ }
+
   return (
     <nav>
-      <ul className='navigation'>
-        <li><NavLink to='/' className='navig'>Home</NavLink></li>
-	<li><NavLink to='/physics' className='navig'>Physics</NavLink></li>
-	<li><NavLink to='/chemistry' className='navig'>Chemistry</NavLink></li>
-        <li><NavLink to='/medicine' className='navig'>Medicine</NavLink></li>
-        <li><NavLink to='/literature' className='navig'>Literature</NavLink></li>
-        <li><NavLink to='/peace' className='navig'>Peace</NavLink></li>
-        <li><NavLink to='/economics' className='navig'>Economics</NavLink></li>
+      <ul className='navbar'>
+        <li><NavLink to='/' className='link' activeClassName="active">Home</NavLink></li>
+        <li className="dropdown">
+          <button type='button' onClick={toggleDropdown} className="button">Categories</button>
+          {dropdownOpen && (
+            <ul className="dropdown-menu">
+              <li><NavLink to='/physics' activeClassName="active">Physics</NavLink></li>
+              <li><NavLink to='/chemistry' activeClassName="active">Chemistry</NavLink></li>
+              <li><NavLink to='/medicine' activeClassName="active">Medicine</NavLink></li>
+              <li><NavLink to='/literature' activeClassName="active">Literature</NavLink></li>
+              <li><NavLink to='/peace' activeClassName="active">Peace</NavLink></li>
+              <li><NavLink to='/economics' activeClassName="active">Economics</NavLink></li>
+            </ul>
+          )}
+        </li>
       </ul>
     </nav>
   );
