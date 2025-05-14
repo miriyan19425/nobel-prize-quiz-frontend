1a50ae413abc1028d24dd3b4de0afc7ede7acbc4
miriyan19425 <miriyan19425@gmail.com>
2025-05-14 23:42:52 +0300

    Create Physics.jsx

---
+++ src/pages/Physics.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Queries from '../components/Queries';
+
+function Physics(){
+	return (
+	 <>
+		<NavBar />
+		<Queries category='physics' />
+	 </>
+    );
+}
+
+export default Physics;
