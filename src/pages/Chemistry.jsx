77b7d13b7cd9f0b628a0f520e299512bb486e7a8
miriyan19425 <miriyan19425@gmail.com>
2025-05-14 23:43:54 +0300

    Create Chemistry.jsx

---
+++ src/pages/Chemistry.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Queries from '../components/Queries';
+
+function Chemistry(){
+	return (
+	 <>
+		<NavBar />
+		<Queries category='chemistry' />
+	 </>
+    );
+}
+
+export default Chemistry;
