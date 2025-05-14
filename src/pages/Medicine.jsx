179604a18e8cef8e15daaf31a474da8f076ac39f
miriyan19425 <miriyan19425@gmail.com>
2025-05-14 23:44:48 +0300

    Create Medicine.jsx

---
+++ src/pages/Medicine.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Queries from '../components/Queries';
+
+function Medicine(){
+	return (
+	 <>
+		<NavBar />
+		<Queries category='medicine' />
+	 </>
+    );
+}
+
+export default Medicine;
