d122783223e72f964f41d4a08548c500fa9d0ce8
miriyan19425 <miriyan19425@gmail.com>
2025-05-15 20:04:40 +0300

    Create Peace.jsx

---
+++ src/pages/Peace.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Queries from '../components/Queries';
+
+function Peace(){
+	return (
+	 <>
+		<NavBar />
+		<Queries category='peace' />
+	 </>
+    );
+}
+
+export default Peace;
