49b8dac45df43685312416ba993c81b6bc776041
miriyan19425 <miriyan19425@gmail.com>
2025-05-15 20:03:53 +0300

    Create Literature.jsx

---
+++ src/pages/Literature.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Queries from '../components/Queries';
+
+function Literature(){
+	return (
+	 <>
+		<NavBar />
+		<Queries category='literature' />
+	 </>
+    );
+}
+
+export default Literature;
