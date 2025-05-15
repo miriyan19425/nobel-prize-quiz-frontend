ff29c57c4eee15caf62f93dd06a9de9d761c532f
miriyan19425 <miriyan19425@gmail.com>
2025-05-15 20:03:05 +0300

    Create Economics.jsx

---
+++ src/pages/Economics.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Queries from '../components/Queries';
+
+function Economics(){
+	return (
+	 <>
+		<NavBar />
+		<Queries category='economics' />
+	 </>
+    );
+}
+
+export default Economics;
