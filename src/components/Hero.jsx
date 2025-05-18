eeabb6639a995c119da932770c5b7a4b913dae7c
miriyan19425 <miriyan19425@gmail.com>
2025-05-18 11:54:04 +0300

    Update and rename Main.jsx to Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,5 @@
+function Hero(){
+    return <main className='hero'><h1>NOBEL PRIZE QUIZ</h1></main>;
+}
+
+export default Hero;
--- src/components/Main.jsx
+++
@@ -1,5 +0,0 @@
-function Main(){
-    return <main><h1>NOBEL PRIZE QUIZ</h1></main>;
-}
-
-export default Main;
