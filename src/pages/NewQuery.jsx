ada29475df7889119c23e33b538e1d41289f8629
miriyan19425 <miriyan19425@gmail.com>
2025-05-18 11:57:22 +0300

    Create NewQuery.jsx

---
+++ src/pages/NewQuery.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import NewQueryForm from '../components/NewQueryForm';
+
+function NewQuery() {
+	return ( 
+    <>
+			  <Header />
+			  <NewQueryForm />	
+		</>
+	);	
+}
+
+export default NewQuery;
