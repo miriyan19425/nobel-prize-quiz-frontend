963cad39d169aed7536a17c627aedd0d0c876021
miriyan19425 <miriyan19425@gmail.com>
2025-05-19 12:26:12 +0300

    Update and rename NewQuery.jsx to NewQuizEntry.jsx

---
+++ src/pages/NewQuizEntry.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import NewQuizEntryForm from '../components/NewQuizEntryForm';
+
+function NewQuizEntry() {
+	return ( 
+        	<>
+			<Header />
+			<NewQuizEntryForm />	
+		</>
+	);	
+}
+
+export default NewQuizEntry;
