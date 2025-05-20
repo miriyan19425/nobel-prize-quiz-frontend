38efd688307d79a7af917cfecb85da2727dff944
miriyan19425 <miriyan19425@gmail.com>
2025-05-20 23:42:58 +0300

    Update Literature.jsx

--- src/pages/Literature.jsx
+++ src/pages/Literature.jsx
@@ -1,11 +1,11 @@
-import NavBar from '../components/NavBar';
-import Queries from '../components/Queries';
+import Header from '../components/Header';
+import QuizEntries from '../components/QuizEntries';
 
 function Literature(){
 	return (
 	 <>
-		<NavBar />
-		<Queries category='literature' />
+		<Header />
+		<QuizEntries category='literature' />
 	 </>
     );
 }
