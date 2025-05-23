7434305c79b41b4cefc5fc47c8aa51a00b4fc2b3
miriyan19425 <miriyan19425@gmail.com>
2025-05-23 20:45:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,57 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from './pages/Home';
+import NewQuizEntry from './pages/NewQuizEntry';
+import Physics from './pages/Physics';
+import Chemistry from './pages/Chemistry';
+import Medicine from './pages/Medicine';
+import Literature from './pages/Literature';
+import Peace from './pages/Peace';
+import Economics from './pages/Economics';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/new',
+    element: <NewQuizEntry />,
+  },
+  {
+    path: '/physics',
+    element: <Physics />,
+  },
+  {
+	  path: '/chemistry',
+    element: <Chemistry />,  
+  },
+  {
+	  path: '/medicine',
+    element: <Medicine />,  
+  },
+  {
+	  path: '/literature',
+    element: <Literature />,  
+  },
+  {
+	  path: '/peace',
+    element: <Peace />,  
+  },
+  {
+	  path: '/economics',
+    element: <Economics />,  
+  },
+]);
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
