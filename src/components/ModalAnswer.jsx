ed866b4ae55bf1c8197a44311106e54a197a9bf8
miriyan19425 <miriyan19425@gmail.com>
2025-06-28 23:14:52 +0300

    Update ModalAnswer.jsx

--- src/components/ModalAnswer.jsx
+++ src/components/ModalAnswer.jsx
@@ -1,11 +1,11 @@
 import styles from './ModalAnswer.module.css';
 
-function ModalAnswer({ answer, handleClose }) {
+function ModalAnswer({ answer, hideAnsWindow }) {
   return (
     <dialog className={styles.modal} open>
       <p className={styles.answer}>{answer}</p>
       <form method="dialog">
-        <button className={styles.closeButton} onClick={handleClose}>Close</button>
+        <button className={styles.closeButton} onClick={hideAnsWindow}>Close</button>
       </form>
     </dialog>
   );
