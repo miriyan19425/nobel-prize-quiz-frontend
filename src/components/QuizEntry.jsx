ffd4cd5c037d2912fe9b1d7822fcc02cb5071953
miriyan19425 <miriyan19425@gmail.com>
2025-06-28 23:17:10 +0300

    Update QuizEntry.jsx

--- src/components/QuizEntry.jsx
+++ src/components/QuizEntry.jsx
@@ -1,13 +1,13 @@
 import styles from './QuizEntry.module.css';
 
-function QuizEntry({question, showAnsWindow, showEditWindow}){
+function QuizEntry({question, showAnsWindow, showEditWindow, delEntry}){
     return (
         <article className={styles.entry}>
-		      <h2 className={styles.question}>{question}</h2>
+	  <h2 className={styles.question}>{question}</h2>
           <section className={styles.buttons}>
             <button type="button" className={styles.answer} onClick={showAnsWindow}>answer</button>
             <button type="button" className={styles.edit} onClick={showEditWindow}>edit</button>
-            <button type="button" className={styles.delete}>delete</button>
+            <button type="button" className={styles.delete} onClick={delEntry}>delete</button>
           </section> 
         </article>
   );
